a97507764e1114d31f802c4a1b762bb0778bef6f
MarcusW <mrvwarnert@gmail.com>
2025-08-13 18:32:13 -0500

    Ephrem and Marcus

---
+++ ractice Creating and Using Objects - 2 - Create a Calculator/Caluclator.js
@@ -0,0 +1,24 @@
+// Calculator
+
+//Step 1: Absolute Value Calculation
+function absoluteValue(num1){
+    return Math.abs(num1);
+}
+console.log(absoluteValue(-115));
+
+//Step 2: Power Calculation
+function powerCalculation(num1, exponent){
+    return Math.pow(num1, exponent);
+}
+console.log(powerCalculation(-2, 3));
+
+//Step 3: Square Root
+function squareRoot(num1){
+    return Math.sqrt(num1);
+}console.log(squareRoot(25));
+
+//Step 4: Maximum and Minimum
+function maximumMimimum(numbers){
+    return Math.max(numbers);
+    return Math.min(numbers);
+}console.log(maximumMimimum(25, 1, 2, 78));
